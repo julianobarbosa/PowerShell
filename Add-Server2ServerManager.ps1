c117ce953619283d9569f4c65383fdccd7866ddc
Juliano Barbosa <julianobarbosa@users.noreply.github.com>
2018-10-17 15:56:58 -0300

    Create Add-Server2ServerManager.ps1

---
+++ Add-Server2ServerManager.ps1
@@ -0,0 +1,17 @@
+$file = get-item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\ServerManager\ServerList.xml"
+
+# Backup current file
+copy-item –path $file –destination $file-backup –force
+
+$xml = [xml] (Get-Content $file)
+
+$Servers = @("gowsap45.pratika.br", "gowsap46.pratika.br") 
+foreach ($server in $Servers) {
+    $newserver = @($xml.ServerList.ServerInfo)[0].clone()
+    $newserver.name = $server
+    $newserver.lastUpdateTime = "0001-01-01T00:00:00"
+    $newserver.status = "2"
+    $xml.ServerList.AppendChild($newserver)
+}
+$xml.Save($file.FullName)
+start-process –filepath $env:SystemRoot\System32\ServerManager.exe –WindowStyle Maximized
