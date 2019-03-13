77884a5221f23afd9badd4afd273e50fbb97a05c
Juliano Barbosa <julianobarbosa@users.noreply.github.com>
2019-03-13 00:31:59 -0300

    Update nonAdmins.ps1

--- nonAdmins.ps1
+++ nonAdmins.ps1
@@ -1,5 +1,6 @@
 # Set directory for installation - Chocolatey does not lock
 # down the directory if not the default
+$env:ChocolateyBinRoot="D:\tools"
 $InstallDir='D:\Tools\Chocolatey'
 $env:ChocolateyInstall="$InstallDir"
 
@@ -19,92 +20,86 @@ choco install ruby.portable -y
 choco install git.commandline -y
 choco feature -h
 choco feature enable -nallowGlobalConfirmation
-cinst GoogleChrome
-cinst sublimetext3
-choco install sublimetext3.powershellalias
-choco install visualstudiocode
-choco install powershell-core
-cinst javaruntime
-cinst java.jdk
-cinst notepadplusplus
-cinst vim
-cinst putty
-cinst sysinternals
-cinst git
-cinst 7zip
-cinst 7zip.commandline
-cinst nodejs
-cinst vlc
-cinst ChocolateyGUI
-cinst Firefox
-cinst PowerShell
-cinst python
-cinst msysgit
-cinst ruby
-cinst ruby.devkit
-cinst flashplayeractivex
-cinst flashplayerplugin
-cinst fiddler
-cinst ccleaner
-cinst filezilla
-cinst paint.net
-cinst windirstat
-cinst autoit.commandline
-cinst ScriptCs
-cinst Compass
-cinst mysql.workbench
-cinst Yeoman
-cinst Wget
-cinst libjpeg-turbo
-cinst curl
-cinst gitextensions
-cinst VirtualCloneDrive
-cinst autohotkey_l
-cinst PDFCreator
-cinst gimp
-cinst libreoffice
-cinst winmerge
-cinst keepass
-cinst cyg-get
-cinst procexp
-cinst wireshark
-cinst FoxitReader
-cinst beyondcompare
-cinst winscp
-cinst greenshot
-cinst PowerGUI
-cinst SourceCodePro
-cinst defraggler
-cinst k-litecodecpackbasic
-cinst nmap
-cinst speccy
-cinst audacity
-cinst googledrive
-cinst TeraCopy
-cinst picasa
-cinst pscx
-cinst mono
-cinst monodevelop
-cinst mp3tag
-cinst ffmpeg
-cinst Recuva
-cinst procmon
-cinst f.lux
-cinst eclipse-java-juno
-cinst PSWindowsUpdate
-cinst cpu-z
-cinst wincdemu
-cinst wincommandpaste
-cinst NimbleText
-choco install vmware-powercli-psmodule
-cinst rktools.2003
-choco install rdm
-choco install vmwareworkstation
-choco install rvtools
-cinst windbg
-cinst CutePDF
-cinst sqliteadmin
-# pick an editor
+cinst GoogleChrome -y
+cinst sublimetext3 -y
+choco install sublimetext3.powershellalias -y
+choco install visualstudiocode -y
+choco install powershell-core -y
+cinst javaruntime -y
+cinst java.jdk -y
+cinst notepadplusplus -y
+cinst vim -y
+cinst putty -y
+cinst sysinternals -y
+cinst git -y
+cinst 7zip -y
+cinst 7zip.commandline -y
+cinst nodejs -y
+cinst vlc -y
+cinst ChocolateyGUI -y
+cinst Firefox -y
+cinst PowerShell -y
+cinst python -y
+cinst msysgit -y
+cinst ruby -y
+cinst ruby.devkit -y
+cinst flashplayeractivex -y
+cinst flashplayerplugin -y
+cinst fiddler -y
+cinst ccleaner -y
+cinst filezilla -y
+cinst paint.net -y
+cinst windirstat -y
+cinst autoit.commandline -y
+cinst ScriptCs -y
+cinst Compass -y
+cinst mysql.workbench -y
+cinst Yeoman -y
+cinst Wget -y
+cinst libjpeg-turbo -y
+cinst curl -y
+cinst gitextensions -y
+cinst VirtualCloneDrive -y
+cinst autohotkey_l -y
+cinst PDFCreator -y
+cinst gimp -y
+cinst libreoffice -y
+cinst winmerge -y
+cinst wireshark -y
+cinst FoxitReader -y
+cinst beyondcompare -y
+cinst winscp -y
+cinst greenshot -y
+cinst PowerGUI -y
+cinst SourceCodePro -y
+cinst defraggler -y
+cinst k-litecodecpackbasic -y
+cinst nmap -y
+cinst speccy -y
+cinst audacity -y
+cinst googledrive -y
+cinst TeraCopy -y
+cinst picasa -y
+cinst pscx -y
+cinst mp3tag -y
+cinst ffmpeg -y
+cinst Recuva -y
+cinst procmon -y
+cinst f.lux -y
+cinst PSWindowsUpdate -y
+cinst cpu-z -y
+cinst wincdemu -y
+cinst wincommandpaste -y
+cinst NimbleText -y
+choco install vmware-powercli-psmodule -y
+cinst rktools.2003 -y
+choco install rdm -y
+choco install vmwareworkstation -y
+choco install rvtools -y
+cinst windbg -y
+cinst CutePDF -y
+cinst sqliteadmin -y
+# pick an editor 
 #choco install visualstudiocode.portable -y # not yet available
 choco install notepadplusplus.commandline -y
 #choco install nano -y
